282c6ff207baf1d17b442b9cfb73c175bc20273f
L-Pircy <taveesha.agarwal@gmail.com>
2023-06-07 02:46:35 +0530

    next layout and routing

--- next-cc/app/about/team/page.jsx
+++ next-cc/app/about/team/page.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Link from "next/link";
 
 const TeamPage = () => {
     return (
         <div>
             <h1>Our Team</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est dolore similique quia corporis voluptatibus tenetur maiores consequuntur, ullam vitae sint eius eum. Quis officia consequuntur vel dolorem accusantium neque voluptatibus quaerat deserunt explicabo? Delectus dignissimos labore voluptatibus explicabo provident dolores eos cupiditate eligendi molestiae neque odio necessitatibus ut tempore consectetur, veritatis pariatur porro suscipit iure illum natus deserunt a voluptas cum! Ab commodi nihil neque adipisci vitae eveniet a voluptates unde obcaecati minus. Facere unde necessitatibus asperiores, a natus praesentium fugiat incidunt. Beatae iste soluta vero reiciendis vel magnam? Porro, perspiciatis! Ipsam vitae quis nihil nulla recusandae esse magni impedit repellat quas, ipsa laborum ad eaque distinctio odio. Odit rerum quasi, exercitationem excepturi voluptatum consequuntur debitis voluptates eligendi repellendus dolore alias doloremque. Nobis accusamus vero quam saepe nam, magni laborum harum. Facilis nesciunt officia itaque magni excepturi illo, obcaecati nihil rem odit cupiditate fuga eius labore nisi, necessitatibus nobis enim!</p>
         </div>
     )
 }
