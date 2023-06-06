3a0eba63e8e3938b9cf19b6ff06fc91c69a7d3ed
L-Pircy <taveesha.agarwal@gmail.com>
2023-06-06 16:17:38 +0530

    recent cc, layout, routing

---
+++ next-cc/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Tm</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi vel blanditiis incidunt maxime sit nobis at, harum molestiae earum nemo dolorem sequi tenetur ab, dolor soluta totam reprehenderit. Aspernatur, vero. Tenetur, officia unde? Vel voluptatum praesentium cumque dolorem quisquam velit facilis voluptate, ad voluptas ipsam vitae obcaecati itaque dolorum non, sint suscipit quae distinctio nostrum quaerat debitis quis sed, similique nisi! Reiciendis inventore id, repudiandae non tempore culpa ea error officiis quibusdam corporis assumenda doloremque earum neque, et officia veritatis est ratione eveniet aut quasi? Asperiores mollitia, magni fuga sit facere quaerat consequuntur laudantium, sequi quibusdam aperiam, in repellat animi.</p>
+        </div>
+    )
+}
+
+export default AboutPage
--- next-cc/app/page.js
+++ next-cc/app/page.js
@@ -1,9 +1,18 @@
-import Image from 'next/image'
+import React from 'react';
+import Link from "next/link";
 
-export default function Home() {
+
+const HomePage = () => {
   return (
     <div>
-      <h1>Welcome</h1>
+      <h1>Welcome to </h1>
+      <ul>
+        <li><Link href='/'>Home</Link></li>
+        <li><Link href='/about'>About</Link></li>
+        <li><Link href='/about/team'>Team</Link></li>
+      </ul>
     </div>
   )
 }
+
+export default HomePage
